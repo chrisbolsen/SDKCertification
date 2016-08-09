4a128067cfb79ded03aa8aa4e98125d15a4838f5
Chris <chrisbolsen@gmail.com>
2016-08-09 11:04:58 -0700

    Add files via upload

---
+++ Demo11.LoanEventPlugin/Demo11PluginClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using EllieMae.Encompass.Automation;
+
+namespace Demo11.LoanEventPlugin
+{
+	///<summary>
+	///Our plugin class, decorated with the Plugin attribute
+	///</summary>
+	[EllieMae.Encompass.ComponentModel.Plugin]
+	public class Demo11PluginClass
+	{
+		///<summary>
+		///A plugin should have a parameterless constructor for subscribing to application events
+		///</summary>
+		public Demo11PluginClass()
+		{
+			EncompassApplication.LoanOpened += EncompassApplication_LoanOpened;
+		}
+
+		//Event handler for the LoanOpened event
+		void EncompassApplication_LoanOpened(object sender, EventArgs e)
+		{
+			//Check to see if the Application Date is empty
+			if (EncompassApplication.CurrentLoan.Fields["745"].IsEmpty())
+			{
+				DialogResult res = MessageBox.Show(EncompassApplication.Screens,
+					"This loan does not have an application date. Fill in today's date?",
+					"Loan Event Plugin", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+				if (res == DialogResult.Yes)
+				{
+					//Set the Application Date field to today's date when the user clicks Yes
+					EncompassApplication.CurrentLoan.Fields["745"].Value = DateTime.Today;
+
+					//Refresh the Loans screen
+					LoansScreen screen = (LoansScreen)EncompassApplication.Screens[EncompassScreen.Loans];
+
+					if (screen.CurrentForm != null)
+						screen.CurrentForm.Refresh();
+				}
+			}
+		}
+	}
+}
---
+++ Demo12.CustomDataPlugin/Demo12PluginClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using EllieMae.Encompass.Automation;
+using EllieMae.Encompass.BusinessObjects;
+
+namespace Demo12.LoanEventPlugin
+{
+	/// <summary>
+	/// Our plugin class, decorated with the Plugin attribute
+	/// </summary>
+	[EllieMae.Encompass.ComponentModel.Plugin]
+	public class Demo12PluginClass
+	{
+		/// <summary>
+		/// A plugin should have a parameterless constructor for subscribing to application events
+		/// </summary>
+		public Demo12PluginClass()
+		{
+			EncompassApplication.LoanOpened += EncompassApplication_LoanOpened;
+		}
+
+		// Event handler for the LoanOpened event
+		void EncompassApplication_LoanOpened(object sender, EventArgs e)
+		{
+			// Retrieve the XML file from the server
+			DataObject customObject = EncompassApplication.Session.DataExchange.GetCustomDataObject("StateFees.xml");
+			
+			// Parse the file as XML
+			XDocument feeDocument = XDocument.Parse(customObject.ToString(Encoding.UTF8));
+			
+			// Check the property state
+			string propertyState = EncompassApplication.CurrentLoan.Fields["14"].FormattedValue;
+
+			// Find the matching fee element in the document, if any
+			var fees = from element in feeDocument.Descendants("FeeItem")
+					   where (string)element.Attribute("State") == propertyState
+					   select (string) element.Attribute("Fee");
+
+			string fee = fees.FirstOrDefault();
+
+			// Save the fee into the loan
+			if (fee != null)
+				EncompassApplication.CurrentLoan.Fields["CUST02FV"].Value = fee;
+		}
+	}
+}
